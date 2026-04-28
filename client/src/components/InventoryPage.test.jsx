f95cad4101efdef354cc3c80736de48e599dd67f
rifflere <82406585+rifflere@users.noreply.github.com>
2026-04-28 14:11:49 -0700

    Add test to useInventory

--- client/src/components/InventoryPage.test.jsx
+++ client/src/components/InventoryPage.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import InventoryPage from '../components/InventoryPage'
 
