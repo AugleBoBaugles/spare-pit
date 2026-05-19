f5e1cecada34b31e3e634892e9b596a43ac1231f
rifflere <82406585+rifflere@users.noreply.github.com>
2026-05-19 15:07:59 -0700

    Add delete route

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -5,5 +5,9 @@ const inventoryRouter = express.Router();
 
 inventoryRouter.get('/inventory', getAllInventory);
 inventoryRouter.post('/inventory', postTool);
+inventoryRouter.delete('/inventory/:id', (req, res) => {
+    const id = req.params.id;
+    res.status(200).send(`Delete inventory item with ID: ${id}`);
+});
 
 export default inventoryRouter;
